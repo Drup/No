f46d506098bc9999843c0ad321c082bc58fc2818
Drup <drupyog@zoho.com>
2016-01-17 01:28:06 +0100

    Tentative use of svg.

--- generator.ml
+++ generator.ml
@@ -1,13 +1,14 @@
 (** Html generation. *)
 
-open Html5
+module H = Html5.M
+module S = Svg.M
 
 
 let words lang =
   Data.distinguish Data.no lang
 
 let head words =
-  M.(head
+  H.(head
       (title @@ pcdata @@ Sequence.head_exn words)
       [
         meta ~a:[ a_charset "utf-8" ] () ;
@@ -15,15 +16,26 @@ let head words =
       ])
 
 let body words =
-  M.(body [
-      div ~a:[ a_id "main" ]
-        [ pcdata @@ Sequence.head_exn words ]
+  H.(body [
+      svg ~a:[
+        S.a_viewbox (0.,0.,400.,100.) ;
+        S.a_id "main" ;
+        S.a_preserveaspectratio "xMidYMid meet" ;
+      ]
+        S.[text
+             ~a:[a_x_list [50.,Some `Percent] ;
+                 a_y_list [50.,Some `Percent] ;
+                 a_text_anchor `Middle ;
+                 a_dominant_baseline `Central ;
+                 a_fill (`Color ("white", None)) ;
+                ]
+             [pcdata @@ Sequence.head_exn words] ]
     ])
 
 
 let html lang =
   let words = words lang in
-  M.html (head words) @@ (body words)
+  H.html (head words) @@ (body words)
 
 let page =
   let advert = "\
@@ -32,7 +44,7 @@ Join the camelians! \240\159\144\170"
   in
   let f doc =
     let b = Buffer.create 17 in
-    P.print ~output:(Buffer.add_string b) ~advert doc ;
+    Html5.P.print ~output:(Buffer.add_string b) ~advert doc ;
     Buffer.contents b
   in
   fun lang -> Lwt.return @@ f @@ html lang
